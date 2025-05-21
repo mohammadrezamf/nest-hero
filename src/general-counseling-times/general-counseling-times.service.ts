4df7f2ffcbd1397899945ec0a79eb12a2e1accf9
780 <mohammadreza.ak@780.team>
2025-05-21 10:03:52 +0330

    change hour

--- src/general-counseling-times/general-counseling-times.service.ts
+++ src/general-counseling-times/general-counseling-times.service.ts
@@ -50,15 +50,7 @@ export class GeneralCounselingTimesService {
       'thursday',
       'friday',
     ];
-    const hours = [
-      '09:00',
-      '10:00',
-      '11:00',
-      '12:00',
-      '13:00',
-      '14:00',
-      '15:00',
-    ];
+    const hours = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
 
     const today = new Date();
 
